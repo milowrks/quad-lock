3c4734316c62350517de11eb624479e722f52c4e
Milo <milozhengz@gmail.com>
2019-08-07 17:32:21 +1000

    build accordion

---
+++ assets/accordion.js
@@ -0,0 +1,20 @@
+(function(){
+  var acc = document.getElementsByClassName("accordion__item");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+  
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.children[1];
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+})();
