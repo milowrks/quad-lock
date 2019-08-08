99283f77b232c7fb2d397e0dd8f5a88a7c09af8c
Milo <milozhengz@gmail.com>
2019-08-08 11:40:30 +1000

    add animation to accordion

--- assets/accordion.js
+++ assets/accordion.js
@@ -7,14 +7,6 @@
       /* Toggle between adding and removing the "active" class,
       to highlight the button that controls the panel */
       this.classList.toggle("active");
-  
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.children[1];
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
     });
   }
 })();
