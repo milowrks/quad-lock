b2082f180a160f729ed0d5ca561841ae36b1b190
Milo <milozhengz@gmail.com>
2019-08-08 13:00:31 +1000

    change transition to max-height based

--- assets/accordion.js
+++ assets/accordion.js
@@ -4,9 +4,8 @@
   
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
       this.classList.toggle("active");
+      // this.querySelector('.panel').classList.toggle("open")
     });
   }
 })();
